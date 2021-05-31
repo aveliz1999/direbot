1ee4f9b39e863794076d191bfc9cec183b0d9166
aveliz1999 <github@veliz99.com>
2021-05-30 21:18:27 -0400

    Add discord settings to bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,13 @@
 import {Client} from 'discord.js-commando';
+import {discordConfig} from "../config";
 
 const client = new Client({
-    owner: '',
-    commandPrefix: ''
+    owner: discordConfig.owner,
+    commandPrefix: discordConfig.prefix
 });
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}`);
 })
 
-client.login();
+client.login(discordConfig.apiToken);
