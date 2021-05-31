014301c35c99949854d1dc670a2663a5d558acd5
aveliz1999 <github@veliz99.com>
2021-05-30 21:37:56 -0400

    Initialize sequelize

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import yaml from "yaml";
+import {Dialect} from "sequelize";
 
 type DiscordConfig = {
     apiToken: string,
@@ -8,4 +9,13 @@ type DiscordConfig = {
     prefix: string,
 }
 
+export type DatabaseConfig = {
+    username: string,
+    password: string,
+    database: string,
+    host: string,
+    dialect: Dialect
+}
+
 export const discordConfig: DiscordConfig = yaml.parse(fs.readFileSync(path.resolve(__dirname, 'discord.yaml'), 'utf-8'));
+export const databaseConfig: DatabaseConfig = yaml.parse(fs.readFileSync(path.resolve(__dirname, 'database.yaml'), 'utf-8'));
---
+++ sequelizeMigrationsConfig.js
@@ -0,0 +1,17 @@
+const yaml = require('yaml');
+const fs = require('fs');
+const path = require('path');
+
+const config = yaml.parse(fs.readFileSync(path.resolve(__dirname, 'config', 'database.yaml'), 'utf-8'));
+
+module.exports = {
+    development: {
+        ...config
+    },
+    test: {
+        ...config
+    },
+    production: {
+        ...config
+    }
+}
