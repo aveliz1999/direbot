7a1a64fe2d1cab94d4db32b871a517a6cc4675ae
aveliz1999 <github@veliz99.com>
2021-05-31 00:25:30 -0400

    Add the AddServer command

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 import {Client} from 'discord.js-commando';
 import {discordConfig} from "../config";
+import AddServerCommand from "./commands/admin/AddServerCommand";
 import initSequelize from "./sequelize";
 
 initSequelize();
@@ -8,6 +9,13 @@ const client = new Client({
     owner: discordConfig.owner,
     commandPrefix: discordConfig.prefix
 });
+
+client.registry
+    .registerGroups([
+        ['admin', 'Administration commands']
+    ])
+    .registerDefaults()
+    .registerCommands([AddServerCommand])
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}`);
---
+++ src/commands/admin/AddServerCommand.ts
@@ -0,0 +1,103 @@
+import {ArgumentCollectorResult, Client, Command, CommandoMessage} from "discord.js-commando";
+import {CategoryChannel, Role, TextChannel} from 'discord.js';
+import MinecraftServer from "../../models/MinecraftServer";
+
+export default class AddServerCommand extends Command {
+
+    constructor(client: Client) {
+        super(client, {
+            name: 'addserver',
+            aliases: ['add-server'],
+            group: 'admin',
+            memberName: 'addserver',
+            description: 'Add a new server',
+            clientPermissions: ['MANAGE_CHANNELS'],
+            args: [
+                {
+                    key: 'name',
+                    prompt: 'What is the name of the new server?',
+                    type: 'string'
+                },
+                {
+                    key: 'authenticatedRole',
+                    prompt: 'What is the authenticated role for this server?',
+                    type: 'role'
+                }
+            ]
+        })
+    }
+
+
+    async run(message: CommandoMessage, args: {name: string, authenticatedRole: Role}, fromPattern: boolean, result?: ArgumentCollectorResult): Promise<any> | null {
+        const channelManager = message.guild.channels;
+
+        let category: CategoryChannel;
+        let statusChannel: TextChannel;
+        let chatChannel: TextChannel;
+
+
+        try {
+            category = await channelManager.create(args.name, {
+                type: 'category'
+            });
+        }
+        catch(err) {
+            return message.reply('Error occurred while creating the category');
+        }
+
+        try {
+            statusChannel = await channelManager.create('Status', {
+                type: 'text',
+                parent: category.id,
+                permissionOverwrites: [
+                    {
+                        id: message.guild.roles.everyone.id,
+                        deny: ['SEND_MESSAGES'],
+                        allow: ['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY']
+                    }
+                ]
+            });
+        }
+        catch(err) {
+            return message.reply('Error occurred while creating the status channel');
+        }
+
+
+        try {
+            chatChannel = await channelManager.create('Game-Chat', {
+                type: 'text',
+                parent: category.id,
+                permissionOverwrites: [
+                    {
+                        id: message.guild.roles.everyone.id,
+                        deny: ['SEND_MESSAGES'],
+                        allow: ['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY']
+                    },
+                    {
+                        id: args.authenticatedRole.id,
+                        allow: ['SEND_MESSAGES']
+                    }
+                ]
+            });
+        }
+        catch(err) {
+            return message.reply('Error occurred while creating the chat channel');
+        }
+
+        try {
+            await MinecraftServer.create({
+                // TODO generate secure API key when creating new server
+                apiKey: '',
+                name: args.name,
+                statusChannel: statusChannel.id,
+                chatChannel: chatChannel.id
+            })
+        }
+        catch(err) {
+            return message.reply('Error occurred while creating the database entry');
+        }
+
+        return message.reply('Successfully created the new server');
+    }
+
+}
