f5121fe502166def3436746f568c1893f4fbf93b
aveliz1999 <github@veliz99.com>
2021-06-08 18:51:23 -0400

    Add amp API

---
+++ src/amp/amp.ts
@@ -0,0 +1,46 @@
+import ampApi from '@cubecoders/ampapi';
+import MinecraftServer from "../models/MinecraftServer";
+
+function ampLog(value: string) {
+    console.log(`[AMP Logs] ${value}`);
+}
+
+async function getLoggedInClient(server: MinecraftServer) {
+    const API = new ampApi.AMPAPI(server.ampServerEndpoint);
+
+    let APIInitOK = await API.initAsync();
+    if(!APIInitOK) {
+        ampLog('API Init failed');
+        return null;
+    }
+
+    const loginResult = await API.Core.LoginAsync(server.ampServerUsername, server.ampServerPassword, '', false);
+    if(loginResult.success) {
+        ampLog('Login success');
+        API.sessionId = loginResult.sessionID;
+
+        APIInitOK = await API.initAsync();
+        if(!APIInitOK) {
+            ampLog('API Stage 2 Init failed');
+            return null;
+        }
+
+        return API;
+    }
+    else {
+        ampLog('Login failed');
+        ampLog(loginResult);
+        return null;
+    }
+}
+
+
+export const startServer = async function (server: MinecraftServer) {
+    const API = await getLoggedInClient(server);
+    await API.Core.StartAsync();
+}
+
+export const killServer = async function (server: MinecraftServer) {
+    const API = await getLoggedInClient(server);
+    await API.Core.StartAsync();
+}
